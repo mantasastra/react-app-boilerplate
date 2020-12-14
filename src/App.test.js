b7a66f62c005211d8c79d981999c0f125a05ad26
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-12-14 05:32:40 +0000

    prepare boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders the app correctly", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeMessage = screen.getByText(/hi there/i);
+  expect(welcomeMessage).toBeInTheDocument();
 });
